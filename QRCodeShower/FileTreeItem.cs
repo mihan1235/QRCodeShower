43dc0470b87a906dcc610cb837c1b258cd615a9d
Mikhail Krivenko <mihan1235@yandex.ru>
2023-03-13 20:40:57 +0300

    Implemented open file

---
+++ QRCodeShower/FileTreeItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+using System.Windows.Forms.Design;
+
+namespace QRCodeShower
+{
+    class FileTreeItem
+    {
+        public FileTreeItem()
+        {
+            this.Items = new ObservableCollection<FileTreeItem>();
+        }
+
+        public string FileName { get; set; }
+
+        public ObservableCollection<FileTreeItem> Items { get; set;}
+    }
+}
--- QRCodeShower/MainWindow.xaml.cs
+++ QRCodeShower/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +12,11 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using forms = System.Windows.Forms;
 
 namespace QRCodeShower
 {
@@ -20,9 +25,105 @@ namespace QRCodeShower
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<FileTreeItem> files = new ObservableCollection<FileTreeItem>();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenFolder(object sender, ExecutedRoutedEventArgs e)
+        {
+            var myDialog = new forms.FolderBrowserDialog();
+            forms.DialogResult result = myDialog.ShowDialog();
+
+            if (result == forms.DialogResult.OK && !string.IsNullOrWhiteSpace(myDialog.SelectedPath))
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(myDialog.SelectedPath);
+                files = createFileTreeItem(dirInfo);
+                FilesTree.ItemsSource = files;
+
+                //MessageBox.Show($"Got files: {myDialog.SelectedPath}");
+            }
+        }
+
+        ObservableCollection<FileTreeItem> createFileTreeItem(DirectoryInfo dirInfo)
+        {
+            var items = new ObservableCollection<FileTreeItem>();
+            FileTreeItem item = new FileTreeItem();
+            item.FileName = dirInfo.Name;
+            foreach (var it in _addDirsItems(dirInfo).Concat(_addFilesItems(dirInfo)))
+            {
+                item.Items.Add(it);
+            }
+
+            items.Add(item);
+            //item.Items = 
+            return items;
+        }
+
+        private List<FileTreeItem> _addDirsItems(DirectoryInfo dirInfo)
+        {
+            var items = new List<FileTreeItem>();
+            foreach (var dir in dirInfo.GetDirectories())
+            {
+                var item = new FileTreeItem();
+                item.FileName = dir.Name;
+                foreach (var it in _addDirsItems(dir).Concat(_addFilesItems(dir)))
+                {
+                    item.Items.Add(it);
+                }
+                items.Add(item);
+            }
+
+            //item.Items = 
+            return items;
+        }
+        List<FileTreeItem> _addFilesItems(DirectoryInfo dirInfo)
+        {
+            var items = new List<FileTreeItem>();
+            foreach (var file in dirInfo.GetFiles())
+            {
+                var item = new FileTreeItem();
+                item.FileName = file.Name;
+                items.Add(item);
+            }
+
+            //item.Items = 
+            return items;
+        }
+
+        private void CanOpenFolder(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CanPauseSlideShow(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = false;
+        }
+
+        private void PlaySlideShow(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        private void CanPlaySlideShow(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = false;
+        }
+
+        private void PauseSlideShow(object sender, ExecutedRoutedEventArgs e)
+        {
+        }
+
+        private void CanStopSlideShow(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = false;
+        }
+
+        private void StopSlideShow(object sender, ExecutedRoutedEventArgs e)
+        {
         }
     }
 }
